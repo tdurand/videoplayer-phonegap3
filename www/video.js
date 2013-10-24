327fb992dbec1f526f87b9a0f6303a5fdf6ebd66
Thibault Durand <tibo.durand@gmail.com>
2013-10-24 11:47:31 -0500

    Fix: videoplayer.js declaration

--- www/video.js
+++ www/video.js
@@ -1,17 +1,15 @@
-cordova.define("cordova/plugin/videoplayer",
-  function(require, exports, module) {
-    var exec = require("cordova/exec");
-    var VideoPlayer = function () {};
+var exec = require("cordova/exec");
 
-    /**
-     * Starts the video player intent
-     *
-     * @param url           The url to play
-     */
-    VideoPlayer.prototype.play = function(url) {
-        exec(null, null, "VideoPlayer", "playVideo", [url]);
-    };
+var VideoPlayer = function () {};
 
-    var videoPlayer = new VideoPlayer();
-    module.exports = videoPlayer;
-});
+/**
+ * Starts the video player intent
+ *
+ * @param url           The url to play
+ */
+VideoPlayer.prototype.play = function(url) {
+    exec(null, null, "VideoPlayer", "playVideo", [url]);
+};
+
+var videoPlayer = new VideoPlayer();
+module.exports = videoPlayer;
